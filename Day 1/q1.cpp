ad76ec9a4e9b97e00bbcfc240f7589544e3f1e61
sulphur-cmd <mechatronics5698@gmail.com>
2026-05-28 22:52:47 +0530

    Some minor imperfections

--- Day 1/q1.cpp
+++ Day 1/q1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     int n,sum=0;
-    cout << "Enter a positive integer: ";
+    cout << "Enter a positive number: ";
     cin >> n;
     for(int i=1;i<=n;++i){
         sum += i;
